89851368b697d758477d327165dffe2316e0391f
Abanoob Morckos <57195233+abanoobmorckos@users.noreply.github.com>
2026-02-10 18:34:59 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -2,14 +2,11 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-# اسم الموديل و مكانه
 MODEL_PATH = "models/MobileNetV2_STREAMLIT.keras"
 model = tf.keras.models.load_model(MODEL_PATH)
 
-# أسماء الكلاسات
 CLASS_NAMES = ["Glioma", "Meningioma", "Pituitary", "No Tumor"]
 
-# دالة لتحويل الصورة وتحضيرها للموديل
 def preprocess_image(uploaded_file):
     image = Image.open(uploaded_file).convert("RGB")
     image = image.resize((224, 224))  # حسب input_shape للموديل
@@ -17,7 +14,6 @@ def preprocess_image(uploaded_file):
     image_array = np.expand_dims(image_array, axis=0)
     return image_array
 
-# دالة التنبؤ
 def predict_image(uploaded_file):
     image = preprocess_image(uploaded_file)
     prediction = model.predict(image)
@@ -25,6 +21,7 @@ def predict_image(uploaded_file):
     confidence = np.max(prediction) * 100
     predicted_class_name = CLASS_NAMES[predicted_class_index]
     return predicted_class_name, confidence
+
 
 
 
